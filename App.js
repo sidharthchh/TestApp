c8413f4c55bb3553c5168e684567626a3b06b869
Sidharth Chhawchharia <sid@Sidharths-MacBook-Pro.local>
2018-01-20 05:19:39 +0530

    Changed the default active View

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default class App extends Component<{}> {
     constructor(props) {
         super(props);
         this.state = {
-            activeView: 'InsightContainer',
+            activeView: 'DashBoardContainer',
             selectedMeeting: null
         }
         this.views = {
